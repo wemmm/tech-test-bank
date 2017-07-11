94fad6a755df88cf880788d2dcf6a2d4a663da32
wemmm <jenny.wem@gmail.com>
2017-07-11 11:41:47 +0100

    Adds Withdrawal class with amount and date, plus make_withdrawal method for Account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,16 @@
+require 'withdrawal'
+
 class Account
 
-  attr_reader :starting_balance
+  attr_reader :starting_balance, :history
 
   def initialize(starting_balance)
     @starting_balance = starting_balance
+    @history = []
+  end
+
+  def make_withdrawal(amount)
+    @history << Withdrawal.new(amount)
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,5 +7,16 @@ describe Account do
     it 'instantiates with a starting balance' do
       expect(account.starting_balance).to eq 2000
     end
+
+    it 'instantiates with a history' do
+      expect(account.history).to eq []
+    end
+  end
+
+  describe '#make_withdrawal' do
+    it 'stores a new Withdrawal object' do
+      account.make_withdrawal(200)
+      expect(account.history.length).to eq 1
+    end
   end
 end
